1dee940c45cda932abf887a47d491d6d846404d1
Calil30 <lucas.laurindo@sptech.school>
2025-06-07 18:12:40 -0300

    Atualização da integração com banco de dados e metade da dashboard funcionando

--- Banco de dados/bd_projetoIndividual.sql
+++ Banco de dados/bd_projetoIndividual.sql
@@ -29,9 +29,57 @@ CREATE TABLE tentativas (
     FOREIGN KEY (usuario_id) REFERENCES usuario(id_usuario) ON DELETE CASCADE
 );
 
+CREATE TABLE respostas (
+  id_resposta INT AUTO_INCREMENT PRIMARY KEY,
+  id_tentativa INT NOT NULL,
+  numero_pergunta INT NOT NULL,
+  correta BOOLEAN NOT NULL,
+  FOREIGN KEY (id_tentativa) REFERENCES tentativas(id_tentativa)
+);
+
 CREATE TABLE estatisticas_gerais (
     id_estatistica INT PRIMARY KEY AUTO_INCREMENT,
     nome_estatistica VARCHAR(100) NOT NULL,
     valor VARCHAR(255) NOT NULL,
     ultima_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+
+-- INSERTS
+INSERT INTO usuario (nome, email, senha) VALUES
+('João Silva', 'joao.silva@email.com', 'senha1234'),
+('Maria Souza', 'maria.souza@email.com', 'minhasenha'),
+('Carlos Pereira', 'carlos.pereira@email.com', 'abc123456'),
+('Ana Lima', 'ana.lima@email.com', 'senhaSegura2025');
+
+
+
+
+
+select * from tentativas;
+
+-- média de acertos do usuário
+SELECT AVG(acertos) AS media_acertos
+FROM tentativas
+WHERE usuario_id = 1;
+
+-- média de erros do usuário
+SELECT AVG(erros) AS media_erros
+FROM tentativas
+WHERE usuario_id = 1;
+
+-- media geral de acertos
+SELECT AVG(acertos) FROM (
+  SELECT COUNT(*) AS acertos
+  FROM respostas
+  WHERE correta = 1
+  GROUP BY id_tentativa
+) AS sub;
+
+-- pergunta mais errada
+SELECT numero_pergunta, COUNT(*) AS erros
+FROM respostas
+WHERE correta = 0
+GROUP BY numero_pergunta
+ORDER BY erros DESC
+LIMIT 1;
